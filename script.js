bfde75c4e118800771fd322d84a22b21c599bf9b
Shah Drashti <112719599+ajshahdrashti@users.noreply.github.com>
2024-06-06 16:30:40 +0530

    script.js

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ const layout = d3.layout.cloud()
     .size([700, 700])
     .words(words.map(d => ({ text: d.text, size: d.size })))
     .padding(1)
-    .rotate(d => d.size > 15 ? 90 : 0) // Rotate words with size > 15 degrees
+    .rotate(d => d.size > 0 ? 90 : 0) // Rotate words with size > 0 degrees
     .fontSize(d => d.size * 5)
     .on("end", draw);
 
