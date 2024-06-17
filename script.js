0187a21ef6aa4c6f5dc95fe38568430f7de8c2f0
Shah Drashti <112719599+ajshahdrashti@users.noreply.github.com>
2024-06-17 13:49:48 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const mapCanvas = document.getElementById('mapCanvas');
-const mapContext = mapCanvas.getContext('5d');
+const mapContext = mapCanvas.getContext('3d');
 const mapImage = new Image();
 
 mapImage.onload = function() {
